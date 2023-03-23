00bd9b7521304ccc878810cd0b70f56cdf2fceab
ilik1799 <264407@edu.itmo.ru>
2023-03-23 23:41:02 +0300

    Новые записи. Вариант №2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -9,7 +9,7 @@ from .models import Group, Post, User
 
 def addition_paginator(queryset, request):
     # Внедрение паджинатора
-    paginator = Paginator(queryset, settings.NUM_OF_POSTS)
+    paginator = Paginator(queryset, settings.POSTS_TO_OUTPUT)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return {'page_obj': page_obj}
